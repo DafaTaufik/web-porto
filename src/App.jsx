04eafe92910d10bb093fadcc9942b4b0e78ba881
DafaTaufik <dafa.taufik@student.umn.ac.id>
2025-02-13 15:29:19 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Navbar} from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+      <div className='bg-afmber-600 h-screen'>
+      </div>
+      <div className='bg-red-400 h-screen'>
+      </div>
+    </>
+  )
+}
+
+export default App
