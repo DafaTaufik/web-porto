bd3aa61f689340f6c8a6ffd9ffa78d5f8d478ce3
DafaTaufik <dafa.taufik@student.umn.ac.id>
2025-02-25 22:57:33 +0700

    Lanyard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Lanyard from "./Lanyard";
 
 export function Home() {
   return (
-    <section >
-        <div className="h-screen flex flex-col pl-10 justify-center bg-amber-600">
-        <h1 className="text-white text-4xl font-bold">hi, iam</h1>
-        <h1 className="text-white text-4xl font-bold">Dafa Taufik</h1>
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="absolute inset-0 z-10">
+        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+      </div>
+
+      <div className="absolute inset-0 z-20 pointer-events-none">
+        <div className="h-full flex flex-col pl-10 justify-center">
+          <h1 className="text-black text-4xl font-bold">hi, iam</h1>
+          <h1 className="text-black text-4xl font-bold">Dafa Taufik</h1>
         </div>
-    </section>
+      </div>
+    </div>
   );
 }
+
+export default Home;
+
+
+// onCreated={({ gl }) => {
+//   gl.setClearColor('#ffffff', 1); // Set white background
+// }}
